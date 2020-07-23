ee55e3f0cb370ff75a27cfee815820242c617a44
Jorge Ocampo <jorge.luis.ocampo.mascaro@gmail.com>
2020-07-22 19:29:05 -0500

    renames and minor tweaks

--- proto/README.md
+++ proto/README.md
@@ -3,8 +3,6 @@
 This folder contains [Protobuf](https://developers.google.com/protocol-buffers/docs/overview)
 schemas for the communication between the client and server.
 
-If you need to use some of these schemas, compile them & publish them.
-
 ## Steps
 Might have to do `chmod +x ./build.sh`
 npm install
--- proto/npm/compileProto.sh
+++ proto/npm/compileProto.sh
@@ -6,8 +6,8 @@ PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROTO_PATH=$PWD/../src
 PROTO_DIR="${PWD}/proto_out"
 
-rm -f "${PWD}/proto-schemas.js"
-rm -f "${PWD}/proto-schemas.d.ts"
+rm -f "${PWD}/proto-objects.js"
+rm -f "${PWD}/proto-objects.d.ts"
 rm -rf "${PROTO_DIR}"
 
 mkdir "${PROTO_DIR}"
@@ -15,9 +15,9 @@ cp -R "${PROTO_PATH}/" "${PROTO_DIR}"
 
 TOP_LEVEL_PROTOS=( $(find ${PROTO_DIR} -name "*.proto") )
 
-# JS
-pbjs --es6 -t static-module --path "${PROTO_DIR}" --out "${PWD}/proto-schemas.js" ${TOP_LEVEL_PROTOS[@]}
+# JavaScript
+pbjs --es6 -t static-module --path "${PROTO_DIR}" --out "${PWD}/proto-objects.js" ${TOP_LEVEL_PROTOS[@]}
 
 # TypeScript
-pbts --out "${PWD}/proto-schemas.d.ts" "${PWD}/proto-schemas.js"
+pbts --out "${PWD}/proto-objects.d.ts" "${PWD}/proto-objects.js"
 
